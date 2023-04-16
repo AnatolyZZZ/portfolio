0135603d52058105be41f6a5be10d24788f1d526
AnatolyZZZ <anatoly.zykov@gmail.com>
2023-04-16 12:27:26 +0300

    mobile dropdown menu added

--- script.js
+++ script.js
@@ -3,25 +3,37 @@ class verticalAnimation  {
         this.object = obj;
         this.step = speed;
         this.direction = 0;
-        this.height = window.getComputedStyle(obj).getPropertyValue('height');
+        const height_string = window.getComputedStyle(obj).getPropertyValue('height');
+        let ind = height_string.indexOf("p")
+        const sub1 = height_string.slice(0,ind)
+        ind = height_string.indexOf(".")
+        const sub2 = ind === -1 ? sub1 : sub1.slice(0, ind)
+        this.height = Number(sub2);
         this.object.style.position = 'absolute';
-        this.offset = 0;
+        this.offset = this.height;
         this.object.style.top = `-${this.offset}.px`;
         this.inertval = 0;
         this.control = control;
-        this.control.addEventListener('click', this.act);
+        this.control.addEventListener('click', this.act.bind(this));
+        this.object.addEventListener('click', this.hide.bind(this));
+    }
+    hide () {
+        this.offset = this.height;
+        this.object.style.top = `-${this.offset}.px`;
+        clearInterval(this.inertval);
+        this.direction = 0;
     }
 
-    makeStep () {
-        this.offset = this.offset + this.step;
-        if (this.offset >= this.height || this.offset <= 0) {
-            this.offset = this.offset <= 0 ? 0 : this.height;
-            clearInterval(this.inertval);
-            this.direction = 0;
-        } 
-    }
-
-    act () {
+   async act () {
+        const makeStep  = () => {
+            this.offset = this.offset + this.direction * this.step;
+            this.object.style.top = `-${this.offset}.px`;
+            if (this.offset >= this.height || this.offset <= 0) {
+                this.offset = this.offset <= 0 ? 0 : this.height;
+                clearInterval(this.inertval);
+                this.direction = 0;
+            } 
+        }
         if (this.direction === 1) {
             this.direction = -1;
         } else if (this.direction === -1) {
@@ -32,4 +44,10 @@ class verticalAnimation  {
         }
     }
 }
+
+const menu_icon = document.querySelector("#burger-menu");
+const mobile_menu = document.querySelector('#mobile-menu');
+
+const menuAnimation = new verticalAnimation(mobile_menu, 2, menu_icon)
+
 
