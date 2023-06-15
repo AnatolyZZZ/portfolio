926635a897e971d5498d3756f63a270709a9f2da
AnatolyZZZ <anatoly.zykov@gmail.com>
2023-06-15 09:12:51 +0300

    form added, cv added css refactored

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ class verticalAnimation  {
         const sub1 = height_string.slice(0,ind)
         ind = height_string.indexOf(".")
         const sub2 = ind === -1 ? sub1 : sub1.slice(0, ind)
-        this.height = Number(sub2);
+        this.height = isNaN(Number(sub2)) ? 214 : Number(sub2);
         this.object.style.position = 'absolute';
         this.offset = this.height;
         this.object.style.top = `-${this.offset}px`;
@@ -51,3 +51,68 @@ const mobile_menu = document.querySelector('#mobile-menu');
 const menuAnimation = new verticalAnimation(mobile_menu, 2, menu_icon)
 
 
+const form = document.querySelector('#contact');
+const name = document.querySelector('#name');
+const email = document.querySelector('#email');
+const company = document.querySelector('#company');
+const msg = document.querySelector('#message');
+const btn = document.querySelector('#submit')
+
+let n='', em='', c='', m='';
+
+name.addEventListener('input', (e) => {
+    n = e.target.value; 
+    if (name.classList.contains('check')) {
+        name.classList.remove('check')
+     }
+    });
+email.addEventListener('input', (e) => {
+    em = e.target.value; 
+    if (email.classList.contains('check')) {
+        email.classList.remove('check')
+        };
+    })
+    
+company.addEventListener('input', (e) => {
+    c = e.target.value;
+    if (company.classList.contains('check')) {
+        company.classList.remove('check')
+    }
+    
+})
+msg.addEventListener('input', (e) => {
+    m = e.target.value;
+    if (msg.classList.contains('check')) {
+        msg.classList.remove('check');
+    } 
+});
+
+
+const checkValues = (e) => {
+    name.classList.add('check');
+    email.classList.add('check');
+    company.classList.add('check');
+    msg.classList.add('check');
+}
+btn.addEventListener('click', checkValues);
+
+const submitForm = (e) => {
+    e.preventDefault();
+    
+
+    const txt = `company: ${c}\n name: ${n}\n email: ${em}\n messege:\n ${m}`;
+    const msgObj = {
+        chat_id : 326180207,
+        text : txt
+    }
+    const para = {
+        method: 'POST',
+        headers : {"Content-Type" : "application/json"},
+        body: JSON.stringify(msgObj)
+    }
+    fetch('https://api.telegram.org/bot6108117392:AAGpZoGzyqrDoDMXEql_F7jgfPvHvW3xqXY/sendMessage',para)
+    .then(window.alert("message sent"))
+    console.log(txt);
+}
+
+form.addEventListener('submit', submitForm);
